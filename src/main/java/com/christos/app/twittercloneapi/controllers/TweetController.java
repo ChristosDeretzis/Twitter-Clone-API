42fc20a06e93163b93003f0e8277f985aa6f3656
Christos Deretzis <deretzischristos@gmail.com>
2021-03-08 20:23:06 +0200

    Improve Tweet Functionality Code

--- src/main/java/com/christos/app/twittercloneapi/controllers/TweetController.java
+++ src/main/java/com/christos/app/twittercloneapi/controllers/TweetController.java
@@ -31,61 +31,27 @@ public class TweetController {
 
     @GetMapping("/tweets/{tweet_id}")
     public Tweet getTweetById(@PathVariable("tweet_id") Long tweet_id) {
-        Tweet tweet = tweetService.getTweetById(tweet_id);
-
-        if (tweet == null){
-            throw new TweetNotFoundException("Tweet with id " + tweet_id + " does not exist.");
-        }
-
-        return tweet;
+        return tweetService.getTweetById(tweet_id);
     }
 
-    @GetMapping("/users/{user_id}/tweets")
+    @GetMapping("/tweets/by-user/{user_id}")
     public List<Tweet> getTweetsByUserId(@PathVariable("user_id") Long id) {
-        User user = userService.getUserById(id);
-
-        if (user == null) {
-            throw new UserNotFoundException("User with id: " + id + " was not found");
-        }
-
-        List<Tweet> userTweets = tweetService.getTweetsByUserId(id);
-        return userTweets;
+        return tweetService.getTweetsByUserId(id);
     }
 
     @PostMapping("/tweets")
     public ResponseEntity<Tweet> createTweet(@RequestParam("user_id") Long id, @RequestParam("text") String content) {
-        User user = userService.getUserById(id);
-
-        if (user == null) {
-            throw new UserNotFoundException("User with id: " + id + " was not found");
-        }
-
-        Tweet newTweet = tweetService.createNewTweet(content, user);
-        return ResponseEntity.status(HttpStatus.OK).body(newTweet);
+        return ResponseEntity.status(HttpStatus.OK).body(tweetService.createNewTweet(content, id));
     }
 
     @PutMapping("/tweets/{tweet_id}")
     public ResponseEntity<Tweet> updateTweet(@RequestBody Tweet newTweet, @PathVariable("tweet_id") Long id){
-        Tweet tweet = tweetService.getTweetById(id);
-
-        if (tweet == null){
-            throw new TweetNotFoundException("Tweet with id " + id + " does not exist.");
-        }
-
-        UpdateJsonUtils.copyNonNullProperties(newTweet, tweet);
-        tweetService.updateTweet(tweet);
-        return ResponseEntity.ok(tweet);
+        return ResponseEntity.status(HttpStatus.OK).body(tweetService.updateTweet(id, newTweet));
     }
 
     @DeleteMapping("/tweets/{tweet_id}")
     public ResponseEntity<Tweet> deleteTweet(@PathVariable("tweet_id") Long id) {
-        Tweet tweet = tweetService.getTweetById(id);
 
-        if (tweet == null){
-            throw new TweetNotFoundException("Tweet with id " + id + " does not exist.");
-        }
-
-        tweetService.deleteTweet(tweet);
-        return ResponseEntity.ok(tweet);
+        return ResponseEntity.status(HttpStatus.OK).body(tweetService.deleteTweet(id));
     }
 }
--- src/main/java/com/christos/app/twittercloneapi/services/TweetService.java
+++ src/main/java/com/christos/app/twittercloneapi/services/TweetService.java
@@ -1,8 +1,11 @@
 package com.christos.app.twittercloneapi.services;
 
+import com.christos.app.twittercloneapi.exceptions.exceptions.TweetNotFoundException;
+import com.christos.app.twittercloneapi.exceptions.exceptions.UserNotFoundException;
 import com.christos.app.twittercloneapi.models.Tweet;
 import com.christos.app.twittercloneapi.models.User;
 import com.christos.app.twittercloneapi.repositories.TweetRepository;
+import com.christos.app.twittercloneapi.utils.UpdateJsonUtils;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,20 +16,40 @@ import java.util.List;
 public class TweetService {
 
     private TweetRepository tweetRepository;
+    private UserService userService;
 
     public List<Tweet> getAllTweets(){
         return tweetRepository.findAll();
     }
 
     public Tweet getTweetById(Long tweet_id) {
-        return tweetRepository.getTweetById(tweet_id);
+        Tweet tweet = tweetRepository.getTweetById(tweet_id);
+
+        if (tweet == null){
+            throw new TweetNotFoundException("Tweet with id " + tweet_id + " does not exist.");
+        }
+
+        return tweet;
     }
 
     public List<Tweet> getTweetsByUserId(Long user_id) {
-        return tweetRepository.getTweetsByUserId(user_id);
+        User user = userService.getUserById(user_id);
+
+        if (user == null) {
+            throw new UserNotFoundException("User with id: " + user_id + " was not found");
+        }
+
+        List<Tweet> tweets = tweetRepository.getTweetsByUserId(user_id);
+        return tweets;
     }
 
-    public Tweet createNewTweet(String content, User user) {
+    public Tweet createNewTweet(String content, Long user_id) {
+        User user = userService.getUserById(user_id);
+
+        if (user == null) {
+            throw new UserNotFoundException("User with id: " + user_id + " was not found");
+        }
+
         Tweet tweet = new Tweet();
         tweet.setUser(user);
         tweet.setContent(content);
@@ -35,11 +58,26 @@ public class TweetService {
         return tweet;
     }
 
-    public void updateTweet(Tweet tweet) {
+    public Tweet updateTweet(Long id, Tweet newTweet) {
+        Tweet tweet = tweetRepository.getTweetById(id);
+
+        if (tweet == null){
+            throw new TweetNotFoundException("Tweet with id " + id + " does not exist.");
+        }
+
+        UpdateJsonUtils.copyNonNullProperties(newTweet, tweet);
         tweetRepository.save(tweet);
+
+        return tweet;
     }
 
-    public void deleteTweet(Tweet tweet) {
+    public Tweet deleteTweet(Long id) {
+        Tweet tweet = tweetRepository.getTweetById(id);
+
+        if (tweet == null){
+            throw new TweetNotFoundException("Tweet with id " + id + " does not exist.");
+        }
         tweetRepository.delete(tweet);
+        return tweet;
     }
 }
